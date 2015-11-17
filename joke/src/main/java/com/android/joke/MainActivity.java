33578d24c1bb684369dcb306591b12494b65211c
Shi Mingfeng <380575115@qq.com>
2015-11-17 14:39:44 +0800

    bY

--- joke/src/main/java/com/android/joke/MainActivity.java
+++ joke/src/main/java/com/android/joke/MainActivity.java
@@ -1,6 +1,9 @@
 package com.android.joke;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -10,10 +13,21 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.android.joke.util.QQUtil;
+import com.tencent.connect.UserInfo;
+import com.tencent.connect.auth.QQToken;
+import com.tencent.tauth.IUiListener;
+import com.tencent.tauth.Tencent;
+import com.tencent.tauth.UiError;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +37,10 @@ public class MainActivity extends AppCompatActivity {
     private TabLayout tablayout;
     private ViewPager viewPager;
     //private int[] tabImg;
+
+    public static Tencent mTencent;
+    // QQ用户信息
+    private UserInfo mInfo;
 
 private MyFragmentAdapter adapter;
     private DrawerLayout drawerLayout;
@@ -85,8 +103,12 @@ private MyFragmentAdapter adapter;
     }
 
     private void setDrawerContent(NavigationView navViewLogin) {
+       mTencent=LoginActivity.mTencent;
         userImg=(ImageView)navViewLogin.findViewById(R.id.imgUsername);
         username=(TextView)navViewLogin.findViewById(R.id.tvUsername);
+
+        updateUserImg();
+
         // -- 左侧抽屉导航视图 菜单 ------------------------------
         // 导航视图中的菜单选中事件
         navViewLogin.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
@@ -108,4 +130,70 @@ private MyFragmentAdapter adapter;
         });
 
     }
+
+    private void updateUserImg() {
+        if (mTencent!=null&&mTencent.isSessionValid()){
+            IUiListener listener=new IUiListener() {
+                @Override
+                public void onError(UiError e) {
+
+                }
+                @Override
+                public void onComplete(final Object response) {
+
+                    Message msg=new Message();
+                    msg.obj=response;
+                    msg.what=0;
+                    mHandler.sendMessage(msg);
+                    new Thread(){
+                        @Override
+                        public void run() {
+                            JSONObject json=(JSONObject)response;
+                            if (json.has("figureurl")){
+                                Bitmap bitmap=null;
+                                try {
+                                    bitmap= QQUtil.getbitmap(json.getString("figureurl_qq_2"));
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                Message msg=new Message();
+                                msg.obj=bitmap;
+                                msg.what=1;
+                                mHandler.sendMessage(msg);
+                            }
+                        }
+                    }.start();
+
+                }
+
+                @Override
+                public void onCancel() {
+
+                }
+            };
+
+           mInfo=new UserInfo(this,mTencent.getQQToken());
+            mInfo.getUserInfo(listener);
+        }
+    }
+    Handler mHandler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what==0){
+                JSONObject response=(JSONObject)msg.obj;
+                if (response.has("nickname")){
+                    try {
+                    username.setVisibility(View.VISIBLE);
+                    username.setText(response.getString("nickname"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }else if (msg.what==1){
+                Bitmap bitmap=(Bitmap)msg.obj;
+                userImg.setImageBitmap(bitmap);
+                userImg.setVisibility(View.VISIBLE);
+            }
+        }
+    };
 }
